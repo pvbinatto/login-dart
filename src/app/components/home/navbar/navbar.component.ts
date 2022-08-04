b8ce380bd99c5d8cfa7decdbe7a76eb24749e894
Paulo Victor Binatto <paulobinatto@gmail.com>
2022-08-04 02:23:18 -0300

    improving navbar component

--- src/app/components/home/navbar/navbar.component.ts
+++ src/app/components/home/navbar/navbar.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { SidebarcontrolService } from 'src/app/services/sidebarcontrol.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  constructor(
+    private toggleService: SidebarcontrolService,
+    private route: Router
+  ) {}
+  @Output() menuToggled = new EventEmitter<boolean>();
 
-  constructor() { }
+  drawer: boolean = true;
 
-  ngOnInit(): void {
+  userLogged = {
+    name: '',
+    email: '',
+    firstName: '',
+    image: '',
+    lastName: ''
   }
 
+  toggle() {
+    this.drawer = !this.drawer;
+    this.toggleService.toggle();
+  }
+
+  drawerEvent($event: any) {
+    this.drawer = $event;
+  }
+
+  logout() {
+    localStorage.clear();
+    this.route.navigate(['/login']);
+  }
+
+  ngOnInit(): void {
+    let user = JSON.parse(localStorage.getItem('data') || '{}');
+    this.userLogged.name = user.name;
+    this.userLogged.email = user.email;
+    this.userLogged.firstName = user.firstName;
+    this.userLogged.image = user.image;
+  }
 }
