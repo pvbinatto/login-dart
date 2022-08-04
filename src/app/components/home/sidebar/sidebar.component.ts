37480c359cc28b6c5e47c48feb04387a6acdd404
Paulo Victor Binatto <paulobinatto@gmail.com>
2022-08-04 02:22:17 -0300

    improving sidebar component

--- src/app/components/home/sidebar/sidebar.component.ts
+++ src/app/components/home/sidebar/sidebar.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
+import { Menu } from 'src/app/models/menu.model';
+import { SidebarcontrolService } from 'src/app/services/sidebarcontrol.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  @ViewChild('sidenav')
+  public sidenav!: MatSidenav;
 
-  constructor() { }
+  constructor(private sideNavService: SidebarcontrolService, private route: Router) {}
 
-  ngOnInit(): void {
+  menu: Menu = [
+    {
+      title: 'Home',
+      icon: 'home',
+      link: '/',
+      color: '#F58227',
+      active: 'active',
+    },
+    {
+      title: 'Meus dados',
+      icon: 'person_pin',
+      link: '/profile',
+      color: '#F58227',
+      active: '',
+    },
+  ];
+
+  active = false;
+
+  showFiller = false;
+
+  
+
+  ngOnInit() {
+    this.sideNavService.sideNavToggleSubject.subscribe(() => {
+      this.sidenav.toggle();
+    });
   }
 
+  logout() {
+    localStorage.clear();
+    this.route.navigate(['/login']);
+  }
 }
