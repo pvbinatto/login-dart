54689813f586ffd1a1d2151e722345accf42868c
Paulo Victor Binatto <paulobinatto@gmail.com>
2022-08-04 02:24:21 -0300

    improving guard service

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -1,30 +1,38 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { User } from '../models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthServiceService {
+  constructor(private http: HttpClient, private route: Router) {}
 
-  constructor(private http: HttpClient) { }
-
-  getHeader(): { headers: HttpHeaders; } {
-    let header = new HttpHeaders({ "Content-Type": "application/json" });
+  getHeader(): { headers: HttpHeaders } {
+    let header = new HttpHeaders({ 'Content-Type': 'application/json' });
     const requestOptions = { headers: header };
     return requestOptions;
   }
 
   isAuthenticated() {
-    return [];
+    if (localStorage.getItem('token')) {
+      this.route.navigate(['/']);
+    }
+    return false;
+  }
+
+  async save() {
+    let resultado = '';
+    let save = await setTimeout(function () {resultado = "Salvo com sucesso"}, 5000);
+    return save;
   }
 
   async authenticate(credentials: User) {
     let result = await this.http
-      .post<any>(
-        `${environment.api}/auth/login`, credentials, this.getHeader()
-      ).pipe();
+      .post<any>(`${environment.api}/auth/login`, credentials, this.getHeader())
+      .pipe();
     return result;
   }
 }
